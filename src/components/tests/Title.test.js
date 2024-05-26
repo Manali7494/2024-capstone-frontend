6611f22501f856bf49718d8c73d3aab1a2223e9f
Manali B <manalibhattacharyya10@gmail.com>
2024-05-25 22:58:23 -0600

    Add Title component and test

---
+++ src/components/tests/Title.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import Title from '../Title';
+
+test('Add title link', () => {
+  render(<Title />);
+  const title = screen.getByText(/Healthy Wealthy/i);
+  expect(title).toBeInTheDocument();
+});
