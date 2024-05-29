96e7401bea341301d73da10f696c559697c9a558
Manali B <manalibhattacharyya10@gmail.com>
2024-05-28 19:04:51 -0600

    Add variables for cognito

---
+++ src/aws-exports.template.js
@@ -0,0 +1,8 @@
+const awsexports = {
+  aws_project_region: 'AWS_PROJECT_REGION',
+  aws_cognito_region: 'AWS_COGNITO_REGION',
+  aws_user_pools_id: 'AWS_USER_POOLS_ID',
+  aws_user_pools_web_client_id: 'AWS_USER_POOLS_WEB_CLIENT_ID',
+};
+
+export default awsexports;
