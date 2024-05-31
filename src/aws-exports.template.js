fd1e492114cedce90c7cc0d2de1ea8df76725391
Manali B <manalibhattacharyya10@gmail.com>
2024-05-30 20:28:49 -0600

    Add aws exports for cognito

---
+++ src/aws-exports.template.js
@@ -0,0 +1,8 @@
+const awsexports = {
+  aws_project_region: 'us-west-1',
+  aws_cognito_region: 'us-east-1',
+  aws_user_pools_id: 'AWS_USER_POOLS_ID',
+  aws_user_pools_web_client_id: 'AWS_USER_POOLS_WEB_CLIENT_ID',
+};
+
+export default awsexports;
