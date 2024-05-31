73e211509988b88c513ce628f3e156a35f1bd722
Manali B <manalibhattacharyya10@gmail.com>
2024-05-30 20:29:10 -0600

    Add test functionality for Signup

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,19 @@
 import React from 'react';
+// eslint-disable-next-line no-unused-vars
+import { signIn, signUp } from 'aws-amplify/auth';
 import Title from './Title';
+
+const testSignUp = async () => {
+  try {
+    const { userId } = await signUp({
+      username: 'test123@gmail.com',
+      password: 'testPassword',
+    });
+    console.log('userId', userId);
+  } catch (err) {
+    console.log('error signing up:', err);
+  }
+};
 
 function Home() {
   return (
@@ -7,6 +21,8 @@ function Home() {
       <div className="App">
         <header className="App-header">
           <Title />
+          <button type="button" onClick={testSignUp}>Sign Up</button>
+
         </header>
       </div>
     </div>
