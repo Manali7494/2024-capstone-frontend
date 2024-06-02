3a5a932654cff072084dd5e20cc0f55995042e94
Manali B <manalibhattacharyya10@gmail.com>
2024-06-02 13:21:41 -0600

    Add login logic

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,49 +1,87 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
+import React, { useState, useEffect } from 'react';
+import {
+  signIn, getCurrentUser,
+  signOut,
+  fetchAuthSession,
+} from 'aws-amplify/auth';
 
-function Login({ onSubmit }) {
+function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const [display, setDisplay] = useState('');
+  const [user, setUser] = useState(null);
+  const setUserDetails = async () => {
+    const userDetails = await getCurrentUser();
+    setUser(userDetails);
+  };
+  const getUser = async () => {
+    try {
+      const authSession = await fetchAuthSession();
+      if (authSession?.userSub) {
+        setUserDetails();
+      }
+    } catch (err) {
+      console.log('Error', err);
+    }
+  };
 
-  const handleSubmit = (event) => {
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  const handleSignOut = async () => {
+    await signOut({ global: true });
+    setUser(null);
+    setDisplay('Signed out successfully');
+  };
+  const handleSubmit = async (event) => {
     event.preventDefault();
     const emailRegex = /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/;
     if (!emailRegex.test(email)) {
-      setError('Invalid email');
+      setDisplay('Invalid email');
       return;
     }
-    onSubmit({ email, password });
+
+    try {
+      await signIn({ username: email, password });
+      setDisplay('Signed in successfully');
+      setUserDetails();
+    } catch (err) {
+      console.log('Error', err);
+    }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="email">
-        Email:
-        <input
-          id="email"
-          type="text"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </label>
-      <label htmlFor="password">
-        Password:
-        <input
-          id="password"
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </label>
-      <button type="submit">Log in</button>
-      {error && <div>{error}</div>}
-    </form>
+    <>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">
+          Email:
+          <input
+            id="email"
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label htmlFor="password">
+          Password:
+          <input
+            id="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <button type="submit">Log in</button>
+        {display && <div>{display}</div>}
+      </form>
+      {user ? (
+        <button type="button" id="signout" onClick={handleSignOut}>Sign Out</button>
+      ) : null}
+    </>
   );
 }
 
-Login.propTypes = {
-  onSubmit: PropTypes.func.isRequired,
-};
+Login.propTypes = {};
 
 export default Login;
