0ba55df59ed234a720c1a96ecadb10752cad0992
Manali B <manalibhattacharyya10@gmail.com>
2024-06-01 22:05:32 -0600

    Add test

---
+++ cypress/e2e/App.cy.js
@@ -0,0 +1,26 @@
+describe('App', () => {
+  it('successfully loads the home page', () => {
+    cy.visit('/');
+  });
+
+  describe('Registration', () => {
+    beforeEach(() => {
+      cy.visit('/register');
+    });
+
+    it('allows a user to register', () => {
+      const username = 'user';
+      const email = 'manalib2010@gmail.com';
+      const password = '!Testpassword100';
+      const phoneNumber = '123-456-3435';
+
+      cy.get('#username').type(username);
+      cy.get('#email').type(email);
+      cy.get('#password').type(password);
+      cy.get('#phoneNumber').type(phoneNumber);
+      cy.get('form').submit();
+
+      cy.contains('Registration Successful').should('exist');
+    });
+  });
+});
--- cypress/e2e/Home.cy.js
+++
@@ -1,5 +0,0 @@
-describe('Home Page', () => {
-  it('successfully loads', () => {
-    cy.visit('/'); // Navigate to the home page
-  });
-});
