eedeeea727aa334a27ae0e0b1a2f9eb0e91c9c77
Manali B <manalibhattacharyya10@gmail.com>
2024-06-16 00:01:29 -0600

    Add fields for new post

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -2,7 +2,40 @@ import React from 'react';
 
 function NewPost() {
   return (
-    <h1> Create Post </h1>
+    <>
+      <h1> Create Post </h1>
+      <form>
+        <label htmlFor="name">
+          Name
+          <input id="name" type="text" />
+        </label>
+        <label htmlFor="description">
+          Description
+          <input id="description" type="text" />
+        </label>
+        <label htmlFor="imageUrl">
+          Image URL
+          <input id="imageUrl" type="file" />
+        </label>
+        <label htmlFor="price">
+          Price
+          <input id="price" type="number" />
+        </label>
+        <label htmlFor="quantity">
+          Quantity
+          <input id="quantity" type="number" />
+        </label>
+        <label htmlFor="purchaseDate">
+          Purchase Date
+          <input id="purchaseDate" type="date" />
+        </label>
+        <label htmlFor="expiryDate">
+          Expiry Date
+          <input id="expiryDate" type="date" />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    </>
   );
 }
 
