cec32aa2cfe4077da129f040e89437b456bea5e2
Manali B <manalibhattacharyya10@gmail.com>
2024-06-29 18:15:33 -0600

    Add design for posts

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,55 +1,107 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import {
+  TextField,
+  Grid, Paper, Typography, Box, InputAdornment,
+  Card, CardActions, CardMedia, CardContent, IconButton,
+  Button,
+
+} from '@mui/material';
+import {
+  Search, Clear,
+} from '@mui/icons-material';
 import config from '../config';
 
-export function Post({ posts }) {
-  const navigate = useNavigate();
+export function Post({ posts, search, setSearch }) {
   return (
-    <div>
-      <h1> Posts </h1>
-      <input
-        type="text"
-        placeholder="Search Posts"
-      />
-      <div className="posts-container">
-        {posts.map((post) => (
-          <div
-            className="post-card"
-            key={post.id}
-          >
+    <Box sx={{ flexGrow: 1, m: 2 }}>
+      <Typography variant="h4" component="div" gutterBottom>
+        Posts
+      </Typography>
 
-            <img src={post.imageUrl} alt={post.name} style={{ width: '100%', height: 'auto' }} />
-            <h3>{post.name}</h3>
-            <p>{post.description}</p>
-            <p>
-              Price: $
-              {post.price}
-              {' '}
-              - Quantity:
-              {post.quantity}
-            </p>
-            <p>
-              Purchase Date:
-              {post.purchaseDate}
-              {' '}
-              - Expiry Date:
-              {post.expiryDate}
-            </p>
-            <button type="button" onClick={() => navigate(`/posts/${post.id}`)}>Detail</button>
-          </div>
-        ))}
-      </div>
-    </div>
+      <Grid container spacing={2} justifyContent="center">
+        <Grid item xs={12} sm={8}>
+          <TextField
+            label="Search"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            fullWidth
+            margin="normal"
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton aria-label="clearSearch" onClick={() => setSearch('')}>
+                    <Clear color="primary" />
+                  </IconButton>
+                  <IconButton aria-label="textSearch" onClick={() => setSearch(search)}>
+                    <Search color="primary" />
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
+        </Grid>
+      </Grid>
+
+      <Paper elevation={3} style={{ marginTop: '100px' }}>
+        <Grid container spacing={4} justifyContent="center">
+          {posts.map((item) => (
+            <Grid item xs={12} key={item.id}>
+              <Card style={{ margin: '0 auto', width: '50vw' }}>
+                <CardMedia
+                  component="img"
+                  height="250px"
+                  image={item.imageUrl}
+                  alt={item.name}
+                />
+                <CardContent>
+                  <Typography variant="h5" component="div">{item.name}</Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Quantity:
+                    {item.quantity}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Price: $
+                    {item.price}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Purchase Date:
+                    {' '}
+                    {item.purchaseDate}
+                  </Typography>
+                  {
+                      item.expiryDate && (
+                      <Typography variant="body2" color="text.secondary">
+                        Expiry Date:
+                        {item.expiryDate}
+                      </Typography>
+                      )
+                    }
+                </CardContent>
+                <CardActions>
+                  <Button size="small" color="primary" component={Link} to={`/posts/${item.id}/edit`}>
+                    Detail
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Paper>
+    </Box>
   );
 }
 
 Post.propTypes = {
   posts: PropTypes.arrayOf(PropTypes.object).isRequired,
+  search: PropTypes.string.isRequired,
+  setSearch: PropTypes.func.isRequired,
 };
 
 export function PostList() {
   const [posts, setPosts] = useState([]);
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -66,7 +118,7 @@ export function PostList() {
     fetchPosts();
   }, []);
 
-  return <Post posts={posts} />;
+  return <Post posts={posts} search={search} setSearch={setSearch} />;
 }
 
 export default PostList;
