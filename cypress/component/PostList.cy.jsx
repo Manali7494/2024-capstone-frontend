1312aeaa92345cdd3d9d12dd1551b12e4fff3e0f
Manali B <manalibhattacharyya10@gmail.com>
2024-06-29 23:00:54 -0600

    Add test

---
+++ cypress/component/PostList.cy.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { PostList } from '../../src/components/PostList';
+
+describe('PostList', () => {
+  beforeEach(() => {
+    cy.mount(
+      <Router>
+        <PostList />
+      </Router>,
+    );
+  });
+
+  it('displays a list of posts', () => {
+    cy.contains('Posts').should('be.visible');
+    cy.get('#search').should('exist');
+    cy.get('[data-testid="card-item-1"]').should('exist');
+  });
+
+  it('filters to a single post when matching post name', () => {
+    cy.get('[data-testid^="card-item"]').its('length').should('be.gt', 1);
+    cy.get('#search').type('apple');
+    cy.get('[data-testid="search-button"]').click();
+
+    cy.get('[data-testid^="card-item"]').should('have.length', 1);
+    cy.contains(/apple/i).should('be.visible');
+  });
+
+  it('displays message when no matching post name', () => {
+    cy.get('[data-testid^="card-item"]').its('length').should('be.gt', 1);
+    cy.get('#search').type('orange');
+    cy.get('[data-testid="search-button"]').click();
+
+    cy.contains('No posts found.').should('be.visible');
+    cy.get('[data-testid^="card-item"]').should('have.length', 0);
+  });
+});
