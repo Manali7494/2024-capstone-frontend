3b05538f7651b3bef904bead5b2865b1b4c5a551
Manali B <manalibhattacharyya10@gmail.com>
2024-07-11 21:22:41 -0600

    Add design

--- src/components/ViewPost.jsx
+++ src/components/ViewPost.jsx
@@ -71,22 +71,24 @@ function ViewPost({ user }) {
           <Typography variant="h5" gutterBottom>
             View Post
           </Typography>
-          <button
+          <Button
             onClick={handleInterestClick}
-            type="button"
+            variant="contained"
             style={{
               backgroundColor: isUserInterested ? 'green' : 'grey',
               color: isUserInterested ? 'white' : 'black',
             }}
           >
-            {' '}
             Interested
-          </button>
-          {
-            isUserInterested && (
-              <button type="button">ContactInformation</button>
-            )
-          }
+          </Button>
+          {isUserInterested && (
+          <Button
+            type="button"
+            variant="contained"
+          >
+            Contact Information
+          </Button>
+          )}
           <Nutrition postId={id} user={user} />
           <TextField
             label="Name"
