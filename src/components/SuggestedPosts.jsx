06767aa671fe47339ab884d1d7e6deef5e13d7de
Manali B <manalibhattacharyya10@gmail.com>
2024-07-26 23:59:34 -0600

    Refactor render logic

--- src/components/SuggestedPosts.jsx
+++ src/components/SuggestedPosts.jsx
@@ -3,14 +3,87 @@ import {
   Box, Card, CardContent, Typography, Grid,
   CardMedia, CardActions, Button,
   Link, Divider,
+  Paper,
 } from '@mui/material';
 import PropTypes from 'prop-types';
 import config from '../config';
 
-export function SuggestedPostsContent({ posts, code = '' }) {
-  if (code) {
-    return console.log('code', code);
+const renderPosts = (posts) => posts.map(({ post }) => (
+  <Grid item xs={12} sm={6} md={4} key={post.id}>
+    <Card style={{ margin: '0 auto', width: '200px' }} data-testid={`card-item-${post.id}`}>
+      <CardMedia
+        component="img"
+        height="100px"
+        image={post.imageUrl || 'https://via.placeholder.com/450?text=No+Image+Available'}
+        alt={post.name}
+      />
+      <CardContent>
+        <Typography variant="h5" component="div">{post.name}</Typography>
+        <Typography variant="body2" color="text.secondary">
+          Quantity:
+          {post.quantity}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Price: $
+          {post.price}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Purchase Date:
+          {' '}
+          {post.purchaseDate}
+        </Typography>
+        {
+           post.expiryDate && (
+           <Typography variant="body2" color="text.secondary">
+             Expiry Date:
+             {post.expiryDate}
+           </Typography>
+           )
+         }
+      </CardContent>
+      <CardActions>
+        <Button size="small" color="primary" component={Link} to={`/posts/${post.id}`}>
+          Detail
+        </Button>
+      </CardActions>
+    </Card>
+  </Grid>
+
+));
+
+const renderContent = ({ code, posts }) => {
+  if (code === 'USER_NO_PREFERENCE') {
+    return (
+      <Paper
+        elevation={3}
+        sx={{
+          padding: '10px',
+          marginTop: '20px',
+          textAlign: 'center',
+        }}
+      >
+        <Typography
+          variant="h6"
+          component="p"
+          sx={{
+            marginTop: '20px',
+          }}
+          gutterBottom
+        >
+          No posts selected. Click on “interested” button on a post to see suggestions
+        </Typography>
+      </Paper>
+
+    );
   }
+
+  return (
+    <Grid container spacing={6} justifyContent="center">
+      {renderPosts(posts)}
+    </Grid>
+  );
+};
+export function SuggestedPostsContent({ posts, code = '' }) {
   return (
     <Box sx={{ flexGrow: 1, padding: 2 }}>
       <Divider />
@@ -24,54 +97,7 @@ export function SuggestedPostsContent({ posts, code = '' }) {
       >
         Suggested Posts
       </Typography>
-
-      <Grid container spacing={6} justifyContent="center">
-        {
-               posts.map(({ post }) => (
-                 <Grid item xs={12} sm={6} md={4} key={post.id}>
-                   <Card style={{ margin: '0 auto', width: '200px' }} data-testid={`card-item-${post.id}`}>
-                     <CardMedia
-                       component="img"
-                       height="100px"
-                       image={post.imageUrl || 'https://via.placeholder.com/450?text=No+Image+Available'}
-                       alt={post.name}
-                     />
-                     <CardContent>
-                       <Typography variant="h5" component="div">{post.name}</Typography>
-                       <Typography variant="body2" color="text.secondary">
-                         Quantity:
-                         {post.quantity}
-                       </Typography>
-                       <Typography variant="body2" color="text.secondary">
-                         Price: $
-                         {post.price}
-                       </Typography>
-                       <Typography variant="body2" color="text.secondary">
-                         Purchase Date:
-                         {' '}
-                         {post.purchaseDate}
-                       </Typography>
-                       {
-                          post.expiryDate && (
-                          <Typography variant="body2" color="text.secondary">
-                            Expiry Date:
-                            {post.expiryDate}
-                          </Typography>
-                          )
-                        }
-                     </CardContent>
-                     <CardActions>
-                       <Button size="small" color="primary" component={Link} to={`/posts/${post.id}`}>
-                         Detail
-                       </Button>
-                     </CardActions>
-                   </Card>
-                 </Grid>
-               ))
-
-          }
-
-      </Grid>
+      {renderContent({ code, posts })}
     </Box>
   );
 }
@@ -89,6 +115,7 @@ SuggestedPostsContent.propTypes = {
   })).isRequired,
   code: PropTypes.string,
 };
+
 SuggestedPostsContent.defaultProps = {
   code: '',
 };
